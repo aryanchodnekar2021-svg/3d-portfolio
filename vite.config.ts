3550832d25f1d0e59151c21539b0fe1457cadd42
aryanchodnekar <aryanchodnekar2021@gmail.com>
2026-06-27 18:51:47 +0530

    Configure automated deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // @ts-ignore
+  base: process.env.GITHUB_ACTIONS ? "/3D-Portfolio/" : "/",
 });
+
